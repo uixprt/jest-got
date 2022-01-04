b5c6455782d486eed499413519cd0fa86997ee54
Asher Indursky <asheri@proftit.com>
2022-01-04 22:30:42 +0200

    api url vars moves to ignored env

--- src/login.test.ts
+++ src/login.test.ts
@@ -1,23 +1,26 @@
 import faker from 'faker';
 import got from 'got';
+import process from 'process';
 
-import { tryCatchRequest } from './utils';
+import {tryCatchRequest} from './utils';
+
+const api = `${process.env.API_PROTOCOL}://${process.env.API_DOMAIN}/api/user/v3/`
 
 it('login - wrong credentials', async () => {
-    const { response, success } = await tryCatchRequest(() =>
-        got({
-            method: 'POST',
-            url: 'https://api.qa.proftit.com/api/user/v3/tokens/',
-            json: {
-                username: faker.internet.userName(),
-                password: faker.internet.password(),
-            },
-            responseType: 'json',
-        })
-    );
-
-    expect(success).toEqual(false);
-    expect(response.status).toEqual(401);
-    expect(response.data.code).toEqual('ERROR_AUTH_WRONG_CREDENTIALS');
-    expect(response.data.message).toEqual('Wrong credentials');
+  const {response, success} = await tryCatchRequest(() =>
+    got({
+      method: 'POST',
+      url: `${api}tokens/`,
+      json: {
+        username: faker.internet.userName(),
+        password: faker.internet.password(),
+      },
+      responseType: 'json',
+    })
+  );
+  
+  expect(success).toEqual(false);
+  expect(response.status).toEqual(401);
+  expect(response.data.code).toEqual('ERROR_AUTH_WRONG_CREDENTIALS');
+  expect(response.data.message).toEqual('Wrong credentials');
 });
